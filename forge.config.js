66c74a721c65b38059cb5d1e0e245588c4685b99
NuttZu <NuttZu.git@hotmail.com>
2025-11-06 12:22:11 +0700

    disable rpm compile option

--- forge.config.js
+++ forge.config.js
@@ -14,10 +14,10 @@ module.exports = {
         setupIcon: 'src/assets/icon.ico', // optional but recommended
       },
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
+    // {
+    //   name: '@electron-forge/maker-zip',
+    //   platforms: ['darwin'],
+    // },
     {
       name: '@electron-forge/maker-deb',
       config: {
@@ -27,10 +27,10 @@ module.exports = {
         },
       },
     },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    // {
+    //   name: '@electron-forge/maker-rpm',
+    //   config: {},
+    // },
   ],
   plugins: [
     {
